572320fde009778bb021ac5e2fc457ab865669b7
browngojo <100536433+browngojo@users.noreply.github.com>
2025-02-12 10:43:08 +1300

    Add files via upload

--- conversion_rouding.py
+++ conversion_rouding.py
@@ -10,17 +10,17 @@ def to_fahrenheit(to_convert):
     item = (to_convert * (9/5)) + 32
     return round_ans(item)
 
-    
+
 # Main Routine / Testing starts here
-to_c_test = [0, 100, -459]
-to_f_test = [0, 100, 40, -273]
-
-for item in to_f_test:
-    ans = to_fahrenheit(item)
-    print(f"{item} C is {ans} F")
-
-print()
-
-for item in to_c_test:
-    ans = to_celsius(item)
-    print(f"{item} F is {ans} C")
+#to_c_test = [0, 100, -459]
+#to_f_test = [0, 100, 40, -273]
+#
+#for item in to_f_test:
+#    ans = to_fahrenheit(item)
+#    print(f"{item} C is {ans} F")
+#
+#print()
+#
+#for item in to_c_test:
+#    ans = to_celsius(item)
+#    print(f"{item} F is {ans} C")
