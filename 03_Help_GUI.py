839df3e662e9c8c7c66726b7c08036b1339e6701
browngojo <100536433+browngojo@users.noreply.github.com>
2025-02-13 12:06:55 +1300

    Add files via upload

--- 03_Help_GUI.py
+++ 03_Help_GUI.py
@@ -13,11 +13,61 @@ class Converter:
         self.temp_frame = Frame(padx=10, pady=10)
         self.temp_frame.grid()
 
+        self.to_help_button = Button(self.temp_frame,
+                                     text="Help / Info",
+                                     bg="#CC6600",
+                                     fg="#FFFFFF",
+                                     font=("Arial", "14", "bold"), width=12,
+                                     command=self.to_help)
+        self.to_help_button.grid(row=1, padx=5, pady=5)
+
     def to_help(self):
-        print("You pushed the help button")
+        DisplayHelp()
+
+class DisplayHelp:
+
+    def __init__(self):
+
+        # Setup dialogue box and background colour
+        background = "#ffe6cc"
+        self.help_box = Toplevel()
+
+        self.help_frame = Frame(self.help_box,
+                                width=300,
+                                height=200,
+                                )
+        self.help_frame.grid()
+
+        self.help_heading_label = Label(self.help_frame,
+                                    text="Help / Info",
+                                    font=("Arial", "14", "bold"))
+        self.help_heading_label.grid(row=0)
+
+        help_text = "To use the program, simply enter the temperature " \
+                    "you wish to convert and then choose to convert to either " \
+                    "degrees Celsius (Centigrade) or Fahrenheit.. \n\n"
 
 
-    
+        self.help_text_label = Label(self.help_frame,
+                                     text=help_text)
+        self.dismiss_button = Button(self.help_frame,
+                                     font=("Arial", "12", "bold"),
+                                     text="Dismiss", bg="#CC8800",
+                                     fg="#FFFFFF", command=self.close_help)
+        self.dismiss_button.grid(row=2, padx=10, pady=10)
+
+        # List and loop to set background colour on
+        # everything except the buttons
+        recolour_list = [self.help_frame, self.help_heading_label,
+                         self.help_text_label]
+
+        for item in recolour_list:
+            item.config(bg=background)
+
+    def close_help(self):
+        self.help_box.destroy()
+
+
 # Main Routine
 if __name__ == "__main__":
     root = Tk()
